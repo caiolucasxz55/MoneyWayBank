5a74fa4cff9dd56b65e68934682c37d8e8791ab7
caiolucasxz55 <caiolucasxz55@gmail.com>
2025-09-16 19:48:34 -0300

    Feat: Bases packages, model classes, controller classes, services classes, account controller

---
+++ src/test/java/com/fiap/MoneyWayBank/MoneyWayBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.MoneyWayBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyWayBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
